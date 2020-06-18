641a5dfda45ef9aec39a5ceea03a56ba81848c8d
K Adam White <kadamwhite@users.noreply.github.com>
2020-06-18 14:52:22 -0400

    Fix location priority issue so rule is applied
    Thanks, nginx. Thanks.

--- modules/media_fallback/manifests/init.pp
+++ modules/media_fallback/manifests/init.pp
@@ -2,34 +2,20 @@
 class media_fallback (
 	$config,
 ) {
-	# $present_or_absent = $config['fallback_media_host'] ? {
-	# 	/http.*/ => 'present',
-	# 	default  => 'absent',
-	# }
-
-	notify { "Config: ${config['media_fallback']}": }
-
-	$config['media_fallback'].each |String $vm_host, String $fallback_host| {
-		# Build a simpler form of the object key to use in the nginx conf filename.
-		$host_slug = regsubst( regsubst( $vm_host, 'https?://', '' ), '\W+', '_', 'G' );
-
-		file { "/etc/nginx/sites-available/${::fqdn}.d/${host_slug}_media_fallback":
-			ensure  => 'present',
-			content => template( 'media_fallback/fallback.nginx.conf.erb' ),
-			notify  => Service[ 'nginx' ],
-			# Set up after nginx is enabled.
-			require => File[ "/etc/nginx/sites-available/${::fqdn}.d" ],
-		}
+	$present_or_absent = $config['media_fallback'] ? {
+		/.*/ => 'present',
+		default  => 'absent',
 	}
 
-	# # Configure nginx.
-	# file { "/etc/nginx/sites-available/${::fqdn}.d/fallback_media_host":
-	# 	ensure  => 'present',
-	# 	content => template( 'fallback_media_host/fallback.nginx.conf.erb', {
-	# 		'fallbackMediaHost' => $config['fallback_media_host']
-	# 	} ),
-	# 	notify  => Service[ 'nginx' ],
-	# 	# Set up after nginx is enabled.
-	# 	require => File[ "/etc/nginx/sites-available/${::fqdn}.d" ],
-	# }
+	notify { "Present: ${present_or_absent}": }
+
+	$fallback_host = $config['media_fallback'];
+
+	file { "/etc/nginx/sites-available/${::fqdn}.d/media_fallback":
+		ensure  => $present_or_absent,
+		content => template( 'media_fallback/fallback.nginx.conf.erb' ),
+		notify  => Service[ 'nginx' ],
+		# Set up after nginx is enabled.
+		require => File[ "/etc/nginx/sites-available/${::fqdn}.d" ],
+	}
 }
